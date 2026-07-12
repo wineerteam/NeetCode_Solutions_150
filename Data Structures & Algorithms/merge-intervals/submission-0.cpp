f35f2871ca19d7ec906b461b5e079b142bc3dd29
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-07-12 07:53:56 +0530

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        int n = intervals.size();
+
+        vector<vector<int>> ans;
+
+        sort(begin(intervals),end(intervals));
+
+        ans.push_back(intervals[0]);
+        
+        for(int i=0; i<n; i++){
+            if( intervals[i][0] <= ans.back()[1]){
+                //overalapping conditions
+                ans.back()[1]=max(intervals[i][1],ans.back()[1]);
+            }
+            else{
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
